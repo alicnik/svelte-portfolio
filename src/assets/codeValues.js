67d9ff91f0c6c9ee3d4acab23f9a4e45ff71e630
Alex Nicholas <alex.nichoals@tocabot.io>
2021-01-17 21:23:46 +0000

    code for codemirror updated

--- src/assets/codeValues.js
+++ src/assets/codeValues.js
@@ -38,6 +38,7 @@ article {
     width: 85%;
     display: flex;
     justify-content: space-between;
+    overflow-y: auto;
 
     div.content {
         max-width: 422px;
@@ -48,6 +49,19 @@ article {
         color: var(--off-white);
         font-size: 1.1rem;
         line-height: 1.5rem;
+    }
+
+    @media only screen and (max-width: 414px) {
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 3rem 0 0;
+        height: 90vh;
+        gap: 2rem;
+
+        div.content {
+            padding-bottom: 0;
+        }
     }
 }
 
@@ -90,12 +104,13 @@ const texts = [
     'Jest',
     'React Testing Library',
 ];
-const options = {
-    radius: 300,
-    maxSpeed: 'slow',
-};
 
 onMount(() => {
+    const isMobile = window.innerWidth <= 414;
+    const options = {
+        radius: isMobile ? 180 : 300,
+        maxSpeed: 'slow',
+    };
     const tagCloud = TagCloud(container, texts, options);
     return () => tagCloud.destroy();
 });
@@ -122,17 +137,22 @@ onMount(() => {
 
 <style>
 section {
-    height: 100%;
+    height: 90vh;
     display: flex;
     justify-content: center;
     color: var(--off-white);
+    overflow-y: auto;
+
+    @media only screen and (max-width: 414px) {
+        height: 82vh;
+    }
 }
 
 article {
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 60%;
+    width: 65%;
     height: 100%;
 
     p {
@@ -140,12 +160,17 @@ article {
         margin: 1rem;
         text-align: center;
     }
+
+    @media only screen and (max-width: 414px) {
+        width: 85%;
+    }
 }
 
 div.cloud {
     cursor: default;
 }
-</style>`;
+</style>
+`;
 
 export const contactCode = `<script>
 import Editor from './Editor.svelte';
@@ -169,6 +194,7 @@ let touched: Touched = {
     message: false,
 };
 let success = false;
+const isMobile = window.innerWidth <= 414;
 
 init('user_Iusuy42LIpjSZSX8bT4a5');
 
@@ -248,7 +274,7 @@ function setAllTouched(value: boolean) {
             id="message"
             name="message"
             bind:value={message}
-            rows="5"
+            rows={isMobile ? 2 : 5}
             required={touched.message}
         />
     </label>
@@ -266,10 +292,11 @@ function setAllTouched(value: boolean) {
 <style>
 section {
     color: var(--off-white);
-    padding: 7rem 10rem;
+    padding: 3rem 10rem;
     display: flex;
     flex-direction: column;
     align-items: center;
+    overflow-y: auto;
 
     h2 {
         font-size: 4rem;
@@ -278,10 +305,22 @@ section {
         font-style: italic;
         margin-bottom: 2rem;
     }
+
+    @media only screen and (max-width: 414px) {
+        padding: 2rem;
+        overflow-y: auto;
+        height: 90vh;
+
+        h2 {
+            margin-bottom: 0;
+            font-size: 3.3rem;
+            padding-left: 1rem;
+        }
+    }
 }
 form {
     display: flex;
-    width: clamp(300px, 20vw, 600px);
+    width: clamp(300px, 25vw, 600px);
     flex-direction: column;
     justify-content: space-around;
     height: 500px;
@@ -317,6 +356,10 @@ form {
             transform: scale(1.03);
         }
     }
+
+    @media only screen and (max-width: 414px) {
+        height: 350px;
+    }
 }
 
 .success {
@@ -325,17 +368,28 @@ form {
 }
 
 div.direct-contact {
-    padding-top: 2rem;
+    padding-top: 1rem;
+
+    p,
+    address {
+        display: inline;
+    }
+    @media only screen and (max-width: 414px) {
+        padding-left: 1rem;
+    }
 }
+
 a {
     color: var(--off-white);
 }
-</style>`;
+</style>
+`;
 
 export const projectCode = `<script>
 import { fade } from 'svelte/transition';
 import Editor from './Editor.svelte';
 import { projectList } from '../assets/project-list';
+import { projectCode } from '../assets/codeValues';
 </script>
 
 <section id="projects" transition:fade>
@@ -353,7 +407,7 @@ import { projectList } from '../assets/project-list';
     </div>
 {/each}
 </section>
-<Editor value="Hello Projects!" />
+<Editor value={projectCode} />
 
 <style>
 section {
@@ -367,6 +421,10 @@ section {
     align-items: center;
     justify-content: space-evenly;
     margin-bottom: 2rem;
+
+    @media only screen and (max-width: 414px) {
+        flex-direction: column;
+    }
 }
 
 .slide-text {
@@ -382,11 +440,21 @@ section {
         display: inline-block;
         color: var(--off-white);
     }
+
+    @media only screen and (max-width: 414px) {
+        width: 80vw;
+    }
 }
 
 .slide-image {
     width: 40vw;
     border-radius: 10px;
     overflow: hidden;
+
+    @media only screen and (max-width: 414px) {
+        width: 80vw;
+        margin-bottom: 1rem;
+    }
 }
-</style>`;
+</style>
+`;
